3037c3e8030bb72eacd0d8101485dc9c8a510811
uorsanjay <38943635+uorsanjay@users.noreply.github.com>
2018-05-03 12:41:56 +0000

    Update login.ps1

--- login.ps1
+++ login.ps1
@@ -1,3 +1,5 @@
+echo ${az_password}
+echo ${az_username}
 $secpasswd = ConvertTo-SecureString ${az_password} -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential (${az_username}, $secpasswd)
 Connect-AzureRmAccount -Credential $cred
