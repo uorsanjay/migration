a72806544f07bd794f08f665dfe08ce0bbc0ea58
uorsanjay <38943635+uorsanjay@users.noreply.github.com>
2018-05-08 14:34:52 +0000

    Update upload.ps1

--- upload.ps1
+++ upload.ps1
@@ -1,10 +1,15 @@
-Set-AWSCredential -AccessKey AKIAJGQOLAO2N5TT7O7A -SecretKey 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 -StoreAs UttamProfile2
+# echo $env:as_AccessKey
+# echo $env:as_SecretKey 
+$localVhdPath=Get-Content -Path filepath.txt
+echo $localVhdPath
+AWSCredential -AccessKey $as_AccessKey -SecretKey $as_SecretKey -StoreAs UttamProfile2
 Write-S3Object -BucketName migration-azure-tcs -File $localVhdPath -Key X:\testrhel.vhd -ProfileName UttamProfile2
 $container1 = New-Object Amazon.EC2.Model.ImageDiskContainer
 $container1.Format = 'VHD'
 $container1.UserBucket = New-Object Amazon.EC2.Model.UserBucket
 $container1.UserBucket.S3Bucket = 'migration-azure-tcs'
-$container1.UserBucket.S3Key = 'testrhel'
+$container1.UserBucket.S3Key = 'X:/testrhel.vhd'
 $params3 = @{ ClientToken = 'idempotencyToken2'; Platform = 'Linux'; LicenseType = 'BYOL'; DiskContainer = $container1 }
 Import-EC2Image @params3 -ProfileName UttamProfile2
 Get-EC2ImportImageTask -ImportTaskId $task.ImportTaskId
+
