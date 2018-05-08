831e0feef8fa991bca1573b00357a5a4fcea8724
uorsanjay <38943635+uorsanjay@users.noreply.github.com>
2018-05-08 14:28:25 +0000

    Update mount.ps1

--- mount.ps1
+++ mount.ps1
@@ -1,1 +1,13 @@
-
+$storageAccount = Get-AzureRmStorageAccount -ResourceGroupName CloudPractice -Name cloudpracticediag625
+$storageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $storageAccount.ResourceGroupName -Name $storageAccount.StorageAccountName | select -first 1).Value
+$storageContext = New-AzureStorageContext -StorageAccountName $storageAccount.StorageAccountName -StorageAccountKey $storageKey
+$share = Get-AzureStorageShare -Name mymigrateshare -Context $storageContext
+$secKey = ConvertTo-SecureString -String $storageKey -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($storageAccount.StorageAccountName, $secKey)
+$shareDrive = New-PSDrive -Name X -PSProvider FileSystem -Root "\\$($storageAccount.StorageAccountName).file.core.windows.net\$($share.Name)" -Credential $credential -Persist
+$vm = Get-AzureRmVM -Name testrhel -ResourceGroupName CloudPractice
+$vhdUri = $vm.StorageProfile.OsDisk.Vhd.Uri
+$localVhdPath = "$($shareDrive.Name):\$($vm.Name).vhd"
+Save-AzureRmVhd -LocalFilePath $localVhdPath -ResourceGroupName $vm.ResourceGroupName -SourceUri $vhdUri -NumberOfThreads 32 -OverWrite
+$localVhdPath > filepath.txt
+$localVhdPath=Get-Content -Path filepath.txt
