64630c76d51fd90b4ce83b8f24601111291bbfc0
uorsanjay <38943635+uorsanjay@users.noreply.github.com>
2018-05-09 10:18:15 +0000

    Update upload.ps1

--- upload.ps1
+++ upload.ps1
@@ -1,6 +1,5 @@
 # echo $env:as_AccessKey
 # echo $env:as_SecretKey 
-
 $secpasswd = ConvertTo-SecureString $env:az_password -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ($env:az_username, $secpasswd)
 Connect-AzureRmAccount -Credential $cred
