5793d7fce628e5447f9c36d70455e9e9225d64c4
uorsanjay <38943635+uorsanjay@users.noreply.github.com>
2018-05-16 15:25:30 +0000

    Update aws.ps1

--- aws.ps1
+++ aws.ps1
@@ -1,6 +1,6 @@
 #$secretkey = ConvertTo-SecureString $Env:AWS_SECRET_ACCESS_KEY -AsPlainText -Force
 #$accesskey = ConvertTo-SecureString $Env:AWS_ACCESS_KEY_ID -AsPlainText -Force
-Set-AWSCredential -AccessKey "$env:AWS_ACCESS_KEY_ID" -SecretKey "$env:AWS_SECRET_ACCESS_KEY"  -StoreAs UttamProfile2
+Set-AWSCredential -AccessKey "$env:AWS_ACCESS_KEY_ID" -SecretKey "$env:AWS_SECRET_ACCESS_KEY" -StoreAs UttamProfile2
 $container = New-Object Amazon.EC2.Model.ImageDiskContainer
 $container.Format = 'VHD'
 $container.UserBucket = New-Object Amazon.EC2.Model.UserBucket
