e4fd64bf1f54b78136e6b9aea9e800ccaf289d64
uorsanjay <38943635+uorsanjay@users.noreply.github.com>
2018-05-18 11:42:58 +0000

    Update status.ps1

--- status.ps1
+++ status.ps1
@@ -3,3 +3,4 @@ Write-Host 'Waiting for image to import...'
 Start-Sleep -Seconds 10
 }
 Write-Host 'Image import complete!'
+echo 'ami_id="'(Get-EC2ImportImageTask -ImportTaskId $task.ImportTaskId).ImageId'"' > variables.tfvars
